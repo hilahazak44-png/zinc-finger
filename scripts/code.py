921e04be6b599ea172336532d4a60af9f23c7535
hilahazak44 <hilahazak44@gmail.com>
2026-02-26 18:32:27 +0200

    Refine Zinc Finger motif pattern and enhance output formatting in protein sequence analysis

--- scripts/code.py
+++ scripts/code.py
@@ -1,9 +1,9 @@
 import re
 
-# תבנית Zinc Finger
-pattern = "C..C.{12}H.{3,4}H"
+# מוטיב Zinc Finger נכון לפי PROSITE
+pattern = r'C.H.[LIVMFY]C.{2}C[LIVMYA]'
 
-# פתיחת הקובץ הקיים אצלך
+# פתיחת קבצים (כי את מריצה מתוך ZING FINGER)
 file = open("data/orf_trans_all.fa.txt", "r")
 out = open("results/yeast_zinc_finger_orf.txt", "w")
 
@@ -14,14 +14,20 @@ counter = 0
 for line in file:
     line = line.strip()
 
+    # אם זו שורת שם חלבון
     if line.startswith(">"):
+
+        # בדיקת הרצף הקודם
         if name != "":
             matches = re.findall(pattern, sequence)
+
             if matches:
                 counter += 1
                 out.write(name + "\n")
+
                 for m in matches:
                     out.write(m + "\n")
+
                 out.write("\n")
 
         name = line
@@ -30,17 +36,25 @@ for line in file:
     else:
         sequence += line
 
-# בדיקה לרצף האחרון
+
+# בדיקת הרצף האחרון בקובץ
 if name != "":
     matches = re.findall(pattern, sequence)
+
     if matches:
         counter += 1
         out.write(name + "\n")
+
         for m in matches:
             out.write(m + "\n")
+
         out.write("\n")
 
+
+# הדפסת מספר החלבונים הכולל
 out.write("Number of proteins containing Zinc Finger Motif: " + str(counter))
 
 file.close()
 out.close()
+
+print("Finished ✅")
